f3ca86735d9c0b0b84efdb7f1f6d898f097f73c3
ADITI RAJ <aditiraj05062005@gmail.com>
2025-11-16 10:29:21 +0530

    Add files via upload

--- types/note.types.ts
+++ types/note.types.ts
@@ -1,11 +1,11 @@
-import { ISessionTypes } from "./sessionTypes"
-import { IUserType } from "./userTypes"
+import { Types } from 'mongoose';
+import { ISessionTypes } from './session.types';
 
 export interface INoteTypes {
-    sessionId:  ISessionTypes
-    userId: IUserType
-    pdfFileId:string
-    createdAt?: Date
-    updatedAt? :Date
-    noteName : string
+  sessionId: string | Types.ObjectId | ISessionTypes;
+  userId: string | Types.ObjectId;
+  pdfFileId: Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+  noteName: string;
 }
--- types/plans.types.ts
+++ types/plans.types.ts
@@ -1,21 +1,9 @@
 export interface IPlans {
-    _id : string
     name: string,
-    orginalPrice: number, 
     offerPrice : number,
+    orginalPrice: number, 
     interval: "monthly" | "yearly",
     features: {title : string , description : string}[],
     isActive: boolean,
     isHighlighted : boolean
-    
 }
-
-export type IPlanError = {
-    name: string
-    orginalPrice: string, 
-    offerPrice : string,
-    interval: string
-    features: { title: string; description: string }[]
-    isActive: string
-    isHighlighted: string
-  }
